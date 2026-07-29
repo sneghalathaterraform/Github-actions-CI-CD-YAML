e973e34719b5ef8d4cb0711e34daa20a92658b98
sneghalathaterraform <santhoshonetouch29@gmail.com>
2026-07-28 20:54:04 -0400

    Update test message for clarity

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ const assert = require('assert');
 const { add } = require('./index.js');
 
 assert.strictEqual(add(2, 3), 5);
-console.log('All tests passed!');
+console.log('All tests passed good!');
